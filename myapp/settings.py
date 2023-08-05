17a066962ba1f2c94b7996519dd6cf57fbad7ff7
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2023-08-05 16:18:59 -0700

    another update

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ee&qm(=cdgmr$j@&cvl5#^%9c-v-05)iqjozbtg=*718pt7j9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'cyclic.com']
 
 
 # Application definition
